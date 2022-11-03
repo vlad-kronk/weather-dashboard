b87a4e9282ca3c5700ba4d9d554979ecebeee521
Jacob Meyers <jacobmeyers6@outlook.com>
2022-11-03 11:27:06 -0700

    dropdown list shows and hides, history updates correctly but dropdown links are not working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,92 @@
 // API key: 335df852494df73f05faf8726e22ed3a
+
+var searchFormEl = document.querySelector(".search-form")
+var searchBarEl = document.querySelector("#searchBarContent")
+var dropdownEl = document.querySelector(".history-dropdown")
+
+var clientSearchHistory = JSON.parse(localStorage.getItem("w-search-hist")) || [];
+localStorage.setItem("w-search-hist", JSON.stringify(clientSearchHistory));
+
+var searchHistoryListEl = document.querySelector(".history-list");
+
+function init() {
+
+    // show/hide the dropdown on search bar focus/unfocus
+    // only show the dropdown if the user has search history
+    searchBarEl.addEventListener("focus", () => { if (clientSearchHistory.length > 0) { dropdownEl.classList.add("show"); }});
+    searchBarEl.addEventListener("blur", () => { dropdownEl.classList.remove("show"); });
+    refreshSearchHistoryElement();
+
+    console.log(clientSearchHistory);
+}
+
+// update the search history with a new item AND update the UI
+function addToSearchHistory(item) {
+    if (clientSearchHistory.length < 8) {
+        // if the history is not full, unshift
+        clientSearchHistory.unshift(item);
+    } else {
+        // if the history is full, delete last item and unshift
+        clientSearchHistory.pop();
+        clientSearchHistory.unshift(item);
+    }
+    // update local storage with new search history and refresh the UI element
+    localStorage.setItem("w-search-hist", JSON.stringify(clientSearchHistory));
+    refreshSearchHistoryElement();
+}
+
+// updates the search history UI
+function refreshSearchHistoryElement() {
+
+    while (searchHistoryListEl.firstChild) {
+        searchHistoryListEl.removeChild(searchHistoryListEl.firstChild);
+    }
+
+    clientSearchHistory = JSON.parse(localStorage.getItem("w-search-hist"))
+    clientSearchHistory.forEach(item => {
+
+        // <li class="history-item">San Francisco</li>
+        var temp = document.createElement("li");
+        temp.classList.add("history-item");
+        temp.textContent = item;
+
+        searchHistoryListEl.appendChild(temp);
+    });
+
+    document.querySelectorAll(".history-item").forEach(item => {
+        item.addEventListener("click", () => {
+            console.log("user clicked " + item.textContent);
+            searchFormEl.children[0].value = item.textContent;
+            // searchFormEl.submit();
+        })
+    })
+}
+
+function onSearch(e) {
+    e.preventDefault();
+    var cityName = e.target.children[0].value;
+
+    // delete user's text
+    e.target.children[0].value = "";
+    
+    getCoords(cityName);
+
+    // add search to local history and update the UI
+    addToSearchHistory(cityName);
+}
+
+function getCoords(cityName) {
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="
+        + cityName + "&limit=5&appid=335df852494df73f05faf8726e22ed3a")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+function main() {
+
+    searchFormEl.addEventListener("submit", onSearch);
+
+}
+
+init();
+main();
